35b6e7f0c1f9de384664db29177a110d65f61950
sharnjit1 <montystar0@gmail.com>
2015-01-19 12:51:02 +0530

    Basicprogramming8
    java work

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+
+
+
+class Apples{
+    
+    public static int id =0 ;
+    
+    
+    int i ;
+  
+  Apples(){
+    
+     i = id++ ;
+    
+      System.out.println(i);
+     
+   }  
+   
+  
+  
+  
+   
+   
+   
+    
+}
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+       
+             Apples boy = new Apples();  
+              Apples girl = new Apples();  
+                Apples father = new Apples();  
+                
+    }
+        
+}
